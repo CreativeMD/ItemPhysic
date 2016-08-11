fa58ee194f751b278d55512df2e74e672c624c64
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-08-11 15:30:34 +0200

    Added ingame config

---
+++ src/main/java/com/creativemd/itemphysic/config/ItemConfigSystem.java
@@ -0,0 +1,20 @@
+package com.creativemd.itemphysic.config;
+
+import com.creativemd.ingameconfigmanager.api.core.TabRegistry;
+import com.creativemd.ingameconfigmanager.api.tab.ModTab;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class ItemConfigSystem {
+	
+	public static ModTab tab = new ModTab("ItemPhysic", new ItemStack(Items.FEATHER));
+	public static ItemPhysicBranch branch;
+	
+	public static void loadConfig()
+	{
+		branch = new ItemPhysicBranch("general");
+		tab.addBranch(branch);
+		TabRegistry.registerModTab(tab);
+	}
+}
