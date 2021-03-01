227d2320d6bd63ffbef9e163acfb3e3a9f4b2115
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-03-01 17:01:13 +0100

    Added client config gui

--- src/main/java/team/creative/itemphysic/ItemPhysic.java
+++ src/main/java/team/creative/itemphysic/ItemPhysic.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import team.creative.creativecore.client.CreativeCoreClient;
 import team.creative.creativecore.common.config.holder.CreativeConfigRegistry;
 import team.creative.creativecore.common.network.CreativeNetwork;
 import team.creative.itemphysic.client.ItemPhysicClient;
@@ -23,36 +24,37 @@ import team.creative.itemphysic.server.ItemPhysicServer;
 
 @Mod(ItemPhysic.MODID)
 public class ItemPhysic {
-	
-	public static final Logger LOGGER = LogManager.getLogger(ItemPhysic.MODID);
-	public static final String MODID = "itemphysic";
-	public static final CreativeNetwork NETWORK = new CreativeNetwork("1.0", LOGGER, new ResourceLocation(ItemPhysic.MODID, "main"));
-	public static ItemPhysicConfig CONFIG;
-	private InstantPickupSerializer lootSerializer;
-	
-	public ItemPhysic() {
-		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::client));
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
-		FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(GlobalLootModifierSerializer.class, this::register);
-	}
-	
-	@OnlyIn(value = Dist.CLIENT)
-	private void client(final FMLClientSetupEvent event) {
-		ItemPhysicClient.init(event);
-	}
-	
-	private void init(final FMLCommonSetupEvent event) {
-		NETWORK.registerType(DropPacket.class);
-		NETWORK.registerType(PickupPacket.class);
-		
-		CreativeConfigRegistry.ROOT.registerValue(MODID, CONFIG = new ItemPhysicConfig());
-		
-		ItemPhysicServer.init(event);
-	}
-	
-	public void register(RegistryEvent.Register<GlobalLootModifierSerializer<?>> event) {
-		lootSerializer = (InstantPickupSerializer) new InstantPickupSerializer().setRegistryName(new ResourceLocation(MODID, "instant"));
-		event.getRegistry().register(lootSerializer);
-	}
-	
+    
+    public static final Logger LOGGER = LogManager.getLogger(ItemPhysic.MODID);
+    public static final String MODID = "itemphysic";
+    public static final CreativeNetwork NETWORK = new CreativeNetwork("1.0", LOGGER, new ResourceLocation(ItemPhysic.MODID, "main"));
+    public static ItemPhysicConfig CONFIG;
+    private InstantPickupSerializer lootSerializer;
+    
+    public ItemPhysic() {
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::client));
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(GlobalLootModifierSerializer.class, this::register);
+    }
+    
+    @OnlyIn(value = Dist.CLIENT)
+    private void client(final FMLClientSetupEvent event) {
+        ItemPhysicClient.init(event);
+        CreativeCoreClient.registerClientConfig(MODID);
+    }
+    
+    private void init(final FMLCommonSetupEvent event) {
+        NETWORK.registerType(DropPacket.class);
+        NETWORK.registerType(PickupPacket.class);
+        
+        CreativeConfigRegistry.ROOT.registerValue(MODID, CONFIG = new ItemPhysicConfig());
+        
+        ItemPhysicServer.init(event);
+    }
+    
+    public void register(RegistryEvent.Register<GlobalLootModifierSerializer<?>> event) {
+        lootSerializer = (InstantPickupSerializer) new InstantPickupSerializer().setRegistryName(new ResourceLocation(MODID, "instant"));
+        event.getRegistry().register(lootSerializer);
+    }
+    
 }
